a7e3da97f58919ab12e4b8c7b8ce6ab4c47300c6
Dell <Dell@DELL>
2023-06-09 09:13:48 +0530

    Registering services with Eureka server

---
+++ service-registry/src/test/java/com/eshopping_zone/kalyani_ijardar/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eshopping_zone.kalyani_ijardar.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
