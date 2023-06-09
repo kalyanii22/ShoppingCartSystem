005221ba3e4d703f439ec5a6de582c5cb6524220
Dell <Dell@DELL>
2023-06-09 13:14:55 +0530

    Using MongoDB

--- product-service/src/main/java/com/eshopping_zone/kalyani_ijardar/product_service/ProductServiceApplication.java
+++ product-service/src/main/java/com/eshopping_zone/kalyani_ijardar/product_service/ProductServiceApplication.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 @EnableMongoRepositories
+@EnableDiscoveryClient
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
